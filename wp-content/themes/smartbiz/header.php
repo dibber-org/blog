a1c8c66832e38e31c33e702937e12bbb81a85d79
Jérémy Huet <jeremy.huet@gmail.com>
2012-08-22 00:25:25 +0200

    Better header with description and good caption while sharing.

--- wp-content/themes/smartbiz/header.php
+++ wp-content/themes/smartbiz/header.php
@@ -1,29 +1,71 @@
-<!DOCTYPE html>
-<html>
-<head <?php language_attributes(); ?>>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
+<?php
+/**
+ * The Header for our theme.
+ *
+ * Displays all of the <head> section and everything up till <div id="main">
+ *
+ * @package WordPress
+ * @subpackage Twenty_Eleven
+ * @since Twenty Eleven 1.0
+ */
+global $smartbiz;
+?><!DOCTYPE html>
+<!--[if IE 6]>
+<html id="ie6" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 7]>
+<html id="ie7" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 8]>
+<html id="ie8" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if !(IE 6) | !(IE 7) | !(IE 8)  ]><!-->
+<html <?php language_attributes(); ?>>
+<!--<![endif]-->
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<meta name="viewport" content="width=device-width" />
+<title><?php
+	/*
+	 * Print the <title> tag based on what is being viewed.
+	 */
+	global $page, $paged;
 
-	<?php global $smartbiz;
-	if ( is_front_page() ) : ?>
-		<title><?php bloginfo( 'name'); ?></title>
-	<?php elseif ( is_404() ) : ?>
-		<title><?php _e( 'Page Not Found', 'smartbiz' ); ?> | <?php bloginfo( 'name'); ?></title>
-	<?php elseif ( is_search() ) : ?>
-		<title><?php printf(__("Search results for '%s'", "smartbiz"), esc_attr(get_search_query())); ?> | <?php bloginfo( 'name'); ?></title>
-	<?php else : ?>
-		<title><?php wp_title($sep=''); ?> | <?php bloginfo( 'name'); ?></title>
-	<?php endif; ?>
-	<link rel="profile" href="http://gmpg.org/xfn/11" />
-	<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>" type="text/css" media="screen" />
-	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-	<meta name="description" content="<?php bloginfo( 'name' ); ?>" >
-	<?php if ((is_single() || is_category() || is_page() || is_home()) && (!is_paged())){} else { ?>
-		<meta name="robots" content="noindex,follow" >
-	<?php } ?>
-	<?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
-	<?php wp_head(); ?>
-	<link rel="alternate" type="application/rss+xml" title="<?php bloginfo( 'name'); ?> RSS Feed" href="<?php bloginfo( 'rss2_url'); ?>" />
-	<?php wp_head(); ?>
+	wp_title( '|', true, 'right' );
+
+	// Add the blog name.
+	bloginfo( 'name' );
+
+	// Add the blog description for the home/front page.
+	$site_description = get_bloginfo( 'description', 'display' );
+	if ( $site_description && ( is_home() || is_front_page() ) )
+		echo " | $site_description";
+
+	// Add a page number if necessary:
+	if ( $paged >= 2 || $page >= 2 )
+		echo ' | ' . sprintf( __( 'Page %s', 'twentyeleven' ), max( $paged, $page ) );
+
+	?></title>
+<link rel="profile" href="http://gmpg.org/xfn/11" />
+<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+<!--[if lt IE 9]>
+<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
+<![endif]-->
+<?php
+	/* We add some JavaScript to pages with the comment form
+	 * to support sites with threaded comments (when in use).
+	 */
+	if ( is_singular() && get_option( 'thread_comments' ) )
+		wp_enqueue_script( 'comment-reply' );
+
+	/* Always have wp_head() just before the closing </head>
+	 * tag of your theme, or you will break many plugins, which
+	 * generally use this hook to add elements to <head> such
+	 * as styles, scripts, and meta tags.
+	 */
+	wp_head();
+?>
 	<script type="text/javascript">
 	/* <![CDATA[ */
 	function resized(){
@@ -52,7 +94,7 @@
 								<h1><a href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a></h1>
 							</div>
 							<div class="desc">
-								<p><?php bloginfo('description'); ?></p>
+								<p><?php // bloginfo('description'); ?></p>
 							</div>
 						</div>
 					</td>
